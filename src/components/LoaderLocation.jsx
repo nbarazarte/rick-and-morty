61e03bea19c4b23f169fdc4e7f41d623b16c375f
Redjorm <redjorm12@gmail.com>
2023-04-17 13:43:47 -0400

    Loader

---
+++ src/components/LoaderLocation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/loaderlocation.css'
+
+const LoaderLocation = () => {
+  return (
+    <div className='loader'>
+        <div class="lds-hourglass"></div>
+    </div>
+  )
+}
+
+export default LoaderLocation
